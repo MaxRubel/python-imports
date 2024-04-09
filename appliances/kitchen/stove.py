626d7604ee5edea3199a6f6ccd4fd39d75dc00d7
max rubel <maxrubel1@gmail.com>
2024-04-09 18:42:00 -0500

    did it

--- appliances/kitchen/stove.py
+++ appliances/kitchen/stove.py
@@ -3,5 +3,5 @@ def Stove(Appliance):
     def __init__(self, color, heat_method="electric"):
         super.__init__(color)
 
-    def make_coffee():
+    def make_coffee(self):
         print("gurgle, gurgle. Ding. Your drug of choice is piping hot and ready!")
